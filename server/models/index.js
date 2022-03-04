3651e256324663a1d04b374d6b1dc4bae9bae820
ryurbsgks <ryurbsgks5114@gmail.com>
2022-03-04 21:47:47 +0900

    Feat: 게시글 작성 기능 추가
    * 게시글 작성 기능 추가

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-    signupPost: require('./users').signup,
-    signoutDelete: require('./users').signout,
-    loginPost: require('./users').login
+	signupPost: require('./users').signup,
+	signoutDelete: require('./users').signout,
+	loginPost: require('./users').login,
+	writingPost: require('./board').writing
 };
