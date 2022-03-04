733c30719f998099eae214fe906c80a927f06ff2
leejiwon <365support@naver.com>
2022-03-04 09:11:19 +0900

    Feat: component 작성
    * banner
    - 메인페이지 배너
    * bigBtn
    - 로그인 버튼
    * bigBtn1
    - 회원가입 버튼
    * loginuser
    - 프로필 드랍다운
    * navbar
    - 네비바

---
+++ client/src/component/bigBtn/BigBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./BigBtn.css"
+
+function BigBtn() {
+  return (
+    <div>
+      <button className="bigBtn">로그인</button>
+    </div>
+  );
+}
+
+export default BigBtn;
---
+++ client/src/component/loginUser/LoginUser.js
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+import "./LoginUser.css"
+
+function LoginUser() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+  const onClick = () => setIsActive(!isActive);
+
+  return (
+    <div className="menuContainer">
+      <button onClick={onClick} className="menuTrigger">
+        <p className="userName" >안녕하세요</p>
+          <img className="userImg" src = "/images/bear22.png "/>
+      </button>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="#A5A3A4" 
+        stroke-width="2" 
+        stroke-linecap="round" 
+        stroke-linejoin="round">
+          <path d="M6 9l6 6 6-6"/>
+      </svg>
+      <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+        <ul>
+          <li><a href="/messages">마이페이지</a></li>
+          <li><a href="/trips">설정</a></li>
+          <li><a href="/saved">로그아웃</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default LoginUser;
+
