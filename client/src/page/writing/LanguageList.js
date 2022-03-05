d6783e71e67c0bc9eb88655cef30fef273d3877f
leejiwon <365support@naver.com>
2022-03-05 12:58:20 +0900

    Feat: 새글쓰기 페이지 추가

---
+++ client/src/page/writing/LanguageList.js
@@ -0,0 +1,22 @@
+const languageList = [
+  { value: "javascript", label: "JavaScript" },
+  { value: "typescript", label: "TypeScript" },
+  { value: "react", label: "React.js" },
+  { value: "node.js", label: "Node.js" },
+  { value: "vue", label: "Vue.js" },
+  { value: "flutter", label: "Flutter" },
+  { value: "java", label: "Java" },
+  { value: "c", label: "C" },
+  { value: "c++", label: "C++" },
+  { value: "c#", label: "C#" },
+  { value: "spring", label: "Spring" },
+  { value: "angular", label: "Angular.js" },
+  { value: "swift", label: "Swift" },
+  { value: "python", label: "Python" },
+  { value: "django", label: "Django" },
+  { value: "go", label: "Go" },
+  { value: "kotlin", label: "Kotlin" },
+  { value: "ruby", label: "Ruby" },
+];
+
+export default languageList;
