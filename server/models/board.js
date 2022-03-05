457e324067609b04a615dcd7f0e2f787331f81d5
ryurbsgks <ryurbsgks5114@gmail.com>
2022-03-06 05:11:30 +0900

    Feat: 게시글 조회 기능 추가
    * 게시글 조회 기능 추가
    - 요청 페이지가 존재하지 않을 경우 응답 추가

--- server/models/board.js
+++ server/models/board.js
@@ -2,7 +2,7 @@ const db = require('../db')
 
 module.exports = {
   writing: (title, body, PRorTP, stack, callback) => {
-    
+
     const queryString = `INSERT INTO contents (users_id, title, body, created_at, PRorTP) VALUES (3, '${title}', '${body}', now(), '${PRorTP}')`
 
     db.query(queryString, (err, result) => {
@@ -47,26 +47,18 @@ module.exports = {
   },
   detail: (contentId, callback) => {
 
-    const queryString = `SELECT id FROM contents WHERE id = ${contentId};`
+    const queryString = `SELECT contents.title, contents.body, contents.created_at, contents.PRorTP, users.picture, users.nickname, category.stack FROM contents
+    LEFT JOIN users ON contents.users_id = users.id 
+    LEFT JOIN contents_category ON contents.id = contents_category.contents_id 
+    LEFT JOIN category ON contents_category.category_id = category.id 
+    WHERE contents.id = ${contentId};`
 
     db.query(queryString, (err, result) => {
       if (err) {
         return console.log(err)
       }
 
-      const queryString = `SELECT contents.title, contents.body, contents.created_at, contents.PRorTP, users.picture, users.nickname, category.stack FROM contents
-      LEFT JOIN users ON contents.users_id = users.id 
-      LEFT JOIN contents_category ON contents.id = contents_category.contents_id 
-      LEFT JOIN category ON contents_category.category_id = category.id 
-      WHERE contents.id = ${contentId};`
-
-      db.query(queryString, (err, result) => {
-        if (err) {
-          return console.log(err)
-        }
-
-        callback(err, result)
-      })
+      callback(err, result)
     })
   }
 }
