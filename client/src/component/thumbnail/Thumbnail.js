575c54c18f4f2f3236e73a3caa9aebb12bd301bf
leejiwon <365support@naver.com>
2022-03-08 15:49:52 +0900

    Feat: 메인페이지 썸네일, 필터 수정

--- client/src/component/thumbnail/Thumbnail.js
+++ client/src/component/thumbnail/Thumbnail.js
@@ -5,7 +5,18 @@ function Thumbnail(props) {
     return (
         <div className="thumbnailSection">
         <div className="thumbnailBoxFlax">
-            <div className="thumbnailBox"></div>
+            <div className="thumbnailBox">
+                <div className="thumbnailBoxTitle">
+                    <div>쉘위코드 팀원을 모집 합니다</div>
+                </div>
+                <div className="thumbnailBoxTag">
+                <button className="thumbnailTagBtn miniBtn filterMiniBtn">Javascript</button>
+                <button className="thumbnailTagBtn miniBtn filterMiniBtn">Typescript</button>
+                <button className="thumbnailTagBtn miniBtn filterMiniBtn">Node.js</button>
+                <button className="thumbnailTagBtn miniBtn filterMiniBtn">Node.js</button>
+                </div>
+
+            </div>
             <div className="thumbnailBox"></div>
             <div className="thumbnailBox"></div>
             <div className="thumbnailBox"></div>
