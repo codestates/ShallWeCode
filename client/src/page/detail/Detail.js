2bb5b1b3cc22cf036f0a476e8430e4061499c5a1
ace0507 <kysys0507@naver.com>
2022-03-10 19:44:14 +0900

    Design: 디자인 수정

--- client/src/page/detail/Detail.js
+++ client/src/page/detail/Detail.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import Navbar from '../../component/navbar/Navbar';
 import ReactMarkdown from 'react-markdown'
 import "./Detail.css"
@@ -11,11 +10,14 @@ import { useLocation } from 'react-router';
 
 function Detail(props) {
 
-  const history = useHistory()
   const [ boardinfo, setBoardinfo ] = useState("")
-  const [ comment, setComment ] = useState("")
   const location = useLocation()
-  const { isLogin, handleLogout, userinfo } = props
+  const { isLogin, handleLogout } = props
+
+  const clickCommentBtn = () => {
+
+  }
+
   const markdown = 
   `
   프로젝트 설명 : 프로젝트 할 팀원을 구했는데 마음에 안드셨던 경험이 있으신가요?\n - 먼저 개발자들이 작성해둔 포트폴리오를 확인해주세요\n \n모집 인원: 1명\n        \n          
@@ -33,35 +35,7 @@ function Detail(props) {
   | a | b |
   | - | - |
   `
-  // let data;
-
-  const createComment = () => {
-    axios.post('http://localhost:4000/comment/writing', {
-      contentId: location.state.contentId,
-      body: comment
-    })
-    .then((res) => {
-      if (res.status === 201) {
-        alert('댓글 작성 완료')
-      }
-    })
-    window.location.replace('/Detail')
-    
-    // (location || window.location || document.location).reload()
-  }
-
-  const handleInputValue = (key) => (e) => {
-
-    setComment({ ...comment, [key]: e.target.value })
-
-  }
-
-  const handleMyPage = () => {
-    history.push({
-      pathname: '/Mypage',
-      state: { userinfo: [{id: boardinfo.userId, nickname: boardinfo.nickname, picture: boardinfo.picture}] }
-  })  }
-
+  let data;
   useEffect(() => {
     axios.get('http://localhost:4000/board/detail', { params: {contentId: location.state.contentId }})
     .then((res) => {
@@ -72,52 +46,50 @@ function Detail(props) {
 
   }, []);
 
-  // console.log(boardinfo.created_at.toLocaleString("ko-KR", {timeZone: "Asia/Seoul"}))
-
   return (
     <div>
-      <Navbar isLogin={isLogin} userinfo={userinfo} handleLogout={handleLogout}/>
-      { !boardinfo || !userinfo ? <div className="section">잘못된 요청입니다.</div>
+      <Navbar isLogin={isLogin} handleLogout={handleLogout}/>
+      { !boardinfo ? <div className="section">잘못된 요청입니다.</div>
         :
       <div className="section">
         <div className="largeSizeFont detailTitle">{boardinfo.title}</div>
         <div className="detailProfile">
-          <div className="detailProfileImg" onClick={handleMyPage}>
-            <img src={boardinfo.picture} style={{"backgroundColor": "#F7F7F7", "width":"100px", "height" : "100px", "border-radius": "50%"}}></img>
+          <div className="detailProfileImg">
+            <div style={{"backgroundColor": "#F7F7F7", "width":"100px", "height" : "100px", "border-radius": "50%"}}></div>
             <div className="detailName">{boardinfo.nickname}</div>
           </div>
           <div className="detailDate">{boardinfo.created_at}</div>
         </div>
 
-        {boardinfo.userId === userinfo[0].id
-        ? <div className="detailBtn">
+        <div className="detailBtn">
           <button className="commentEditDeleteButton">수정</button>
           <button className="commentEditDeleteButton">삭제</button>
         </div>
-        : <div></div>
-        }
+        
+        
         <div className="detailStack">
-          <div className="detailLanguageMargin">사용언어</div>
+          <div className="detailLanguageMargin">사용언어:</div>
           {boardinfo.stack.map((data, i) => {
-            return <button key={i} className="miniBtn filterMiniBtn">{data}</button>
+            return <button key={i} className="miniBtn filterMiniBtn detailFilterBtn">{data}</button>
           })
         }
         </div>
         <div className="thinLine"></div>
-        <div>
+        <div className="detailText">
           <Viewer initialValue={boardinfo.body} />
         </div> 
-        <CommentList contentId={location.state.contentId} userinfo={userinfo}/>
+        
+        <CommentList contentId={location.state.contentId}/>
+        <h2>댓글</h2>
         <div className="commentBox">
           <div className="detailComment">
-            <img src={userinfo[0].picture} style={{"backgroundColor": "#F7F7F7", "width":"40px", "height" : "40px", "border-radius": "50%"}} />
-            <div className="commentName">{userinfo[0].nickname}</div>
+            <div style={{"backgroundColor": "#F7F7F7", "width":"40px", "height" : "40px", "border-radius": "50%"}} />
+            <div className="commentName">닉네임</div>
           </div>
-          <input className="commentInput" type="text" placeholder="입력하세요!" onChange={handleInputValue('comment')}/>
+          <input className="commentInput" type="text" placeholder="입력하세요!" />
         </div> 
         <div className="commentBtn">
-          <button className="miniBtn writingCancelBtn smallSizeFont" onClick={createComment}>입력</button>
-
+          <button onClick={clickCommentBtn} className="miniBtn writingCancelBtn smallSizeFont" >등록</button>
         </div>
       </div> 
       }
